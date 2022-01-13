8016bafc10396421a59dc7b8bd39eb813e73382e
Gergerov <gergerov94@gmail.com>
2022-01-13 23:10:16 +0300

    Фильтрация продуктов + пагинация
    Сделан отдельный класс для фильтрации продуктов с описанием.
    Пагинации доработаны в части описания и добавлен параметр размера страницы

--- main/berg/views.py
+++ main/berg/views.py
@@ -1,8 +1,6 @@
 from django.shortcuts import render
 from rest_framework.viewsets import ModelViewSet
 from rest_framework.permissions import IsAdminUser
-from rest_framework import filters
-from django_filters.rest_framework import DjangoFilterBackend
 from berg.serializers import (
   UnitSerializer, NutrientSerializer, ProductSerializer
 )
@@ -12,6 +10,7 @@ from berg.models import (
 from berg.paginations import (
   UnitPagination, NutrientPagination, ProductPagination
 )
+from berg.filters import ProductFilter
 
 
 class UnitViewSet(ModelViewSet):
@@ -33,15 +32,13 @@ class NutrientViewSet(ModelViewSet):
 
 
 class ProductViewSet(ModelViewSet):
-  """Представление продуктов"""
+  """Представление продуктов. Search - поиск по названию."""
 
   serializer_class = ProductSerializer
   queryset = Product.products.all()
   pagination_class = ProductPagination
   permission_classes = [IsAdminUser]
   filter_backends = [
-    # DjangoFilterBackend, 
-    filters.SearchFilter
+    ProductFilter
   ]
-  # filterset_fields = ['product_name',]
   search_fields = ['^product_name',]
